ad22a8c2980a2b049843422dc8fe10755f178166
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-17 15:56:52 +0200

    MapView

---
+++ SwiftUI Tutorials/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  SwiftUI Tutorials
+//
+//  Created by Natalia Nikiforuk on 17/10/2024.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ SwiftUI Tutorials/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  SwiftUI Tutorials
+//
+//  Created by Natalia Nikiforuk on 17/10/2024.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
