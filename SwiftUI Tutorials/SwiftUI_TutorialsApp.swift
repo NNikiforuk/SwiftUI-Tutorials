fb5d77743ca0c86ccc8872a5426cefd53aa322bc
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-17 14:50:46 +0200

    Initial Commit

---
+++ SwiftUI Tutorials/SwiftUI_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialsApp.swift
+//  SwiftUI Tutorials
+//
+//  Created by Natalia Nikiforuk on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
